866b71fb0441495da5ff89f8f9a789dcffcc1941
Alex Kogan <akautosport@yahoo.com>
2018-12-28 19:34:21 +0000

    first commit

---
+++ charts.js
@@ -0,0 +1,339 @@
+// ******** Main Functions ********
+
+// ******** HOME Page ********
+
+// Upcoming Table
+
+function ajaxPostEmail(data){
+ var request = new XMLHttpRequest();
+ request.open("POST", "/submit");
+ request.send(data);
+}
+
+
+function upcoming_table(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                var launch_data = get_vals_upcoming(this.response);
+                Plotly.plot('upcoming_chart', launch_data.upcoming_launches, launch_data.layout);
+            }
+        };
+        xhttp.open("GET", "/upcoming");
+        xhttp.send();
+}
+
+function past_table(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                var launch_data = get_vals_past(this.response);
+                Plotly.plot('past_chart', launch_data.upcoming_launches, launch_data.layout);
+            }
+        };
+        xhttp.open("GET", "/past");
+        xhttp.send();
+}
+
+// Latest Table
+function latest_table(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                var launch_data = get_vals_latest(this.response);
+                Plotly.plot('latest_chart', launch_data.upcoming_launches, launch_data.layout);
+            }
+        };
+        xhttp.open("GET", "/latest");
+        xhttp.send();
+}
+
+// ******** All Launches Page ********
+function all_past_table(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                var launch_data = get_vals_allPast(this.response);
+                Plotly.plot('allPast_chart', launch_data.upcoming_launches, launch_data.layout);
+            }
+        };
+        xhttp.open("GET", "/allPast");
+        xhttp.send();
+}
+
+
+function site_pie(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                var pie_data = launch_site_pie(this.response);
+                console.log("data:" + pie_data);
+                Plotly.plot('site_pie_chart', pie_data.data, pie_data.layout);
+            }
+        };
+        xhttp.open("GET", "/site_count");
+        xhttp.send();
+}
+
+// ******** Failed Launches Page ********
+
+
+function all_failed_table(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                var launch_data = get_vals_allPast_failed(this.response);
+                Plotly.plot('fail_chart', launch_data.upcoming_launches, launch_data.layout);
+            }
+        };
+        xhttp.open("GET", "/failed");
+        xhttp.send();
+}
+
+// ********  Supporting Functions ********
+
+function data_sort(raw_data){
+    var result = [ [], [], [], [], [], [], []];
+    for (var x of raw_data){
+        result[0].push(String(x[0]));
+        result[1].push(String(x[1]));
+        result[2].push(String(x[2]));
+        result[3].push(String(x[3]));
+        result[4].push(String(x[4]));
+        result[5].push(String(x[5]));
+        result[6].push(String(x[6]));
+    }
+    return result; 
+}
+
+
+// Sorting for fail table (additional list in result)
+function data_sort_2(raw_data){
+    var result = [ [], [], [], [], [], [], [], []];
+    for (var x of raw_data){
+        result[0].push(String(x[0]));
+        result[1].push(String(x[1]));
+        result[2].push(String(x[2]));
+        result[3].push(String(x[3]));
+        result[4].push(String(x[4]));
+        result[5].push(String(x[5]));
+        result[6].push(String(x[6]));
+        result[7].push(String(x[7]));
+    }
+    return result; 
+}
+
+
+function get_vals_upcoming(json){
+    var data = JSON.parse(json);
+    var cell_vals = data_sort(data);
+    var launch_data = {
+        upcoming_launches: [{
+            type: 'table',
+            columnwidth: [30, 30, 40, 30, 30, 30, 40],
+            header: {
+                values: [["Flight Number"], ["Mission Name"], ["Launch Date"],
+                			["Cargo"], ["Cargo Weight"], ["Cargo Owner"], ["Launch Site"]],
+                align: "center",
+                height: 40,
+                line: {width: 4, color: 'black'},
+                fill: {color: "#ffb200"},
+                font: {family: "Arial", size: 18, color: "white"}
+            },
+            cells: {
+                values: cell_vals,
+                align: "center",
+                height: 30,
+                line: {color: "black", width: 4},
+                fill: {color: "#666699"},
+                font: {family: "Arial", size: 12, color: ["white"]}
+            }
+        }],
+        layout: {
+            title: 'Next 10 Launches',
+            autosize: true,
+            height: 840,
+            paper_bgcolor: '#000000',
+            plot_bgcolor: '#000000'
+        }
+    };
+    return launch_data;
+}
+
+function get_vals_past(json){
+    var data = JSON.parse(json);
+    var cell_vals = data_sort(data);
+    var launch_data = {
+        upcoming_launches: [{
+            type: 'table',
+            columnwidth: [30, 30, 40, 30, 30, 30, 40],
+            header: {
+                values: [["Flight Number"], ["Mission Name"], ["Launch Date"],
+                			["Cargo"], ["Cargo Weight"], ["Cargo Owner"], ["Launch Site"]],
+                align: "center",
+                height: 40,
+                line: {width: 4, color: 'black'},
+                fill: {color: "#ffb200"},
+                font: {family: "Arial", size: 18, color: "white"}
+            },
+            cells: {
+                values: cell_vals,
+                align: "center",
+                height: 30,
+                line: {color: "black", width: 4},
+                fill: {color: "#666699"},
+                font: {family: "Arial", size: 12, color: ["white"]}
+            }
+        }],
+        layout: {
+            title: 'Last 10 Launches',
+            autosize: true,
+            height: 840,
+            paper_bgcolor: '#000000',
+            plot_bgcolor: '#000000'
+        }
+    };
+    return launch_data;
+}
+
+
+function get_vals_latest(json){
+    var data = JSON.parse(json);
+    var cell_vals = data_sort(data);
+    var launch_data = {
+        upcoming_launches: [{
+            type: 'table',
+            columnwidth: [30, 30, 40, 30, 30, 30, 40],
+            header: {
+                values: [["Flight Number"], ["Mission Name"], ["Launch Date"],
+                			["Cargo"], ["Cargo Weight"], ["Cargo Owner"], ["Launch Site"]],
+                align: "center",
+                height: 40,
+                line: {width: 4, color: 'black'},
+                fill: {color: "#ffb200"},
+                font: {family: "Arial", size: 18, color: "white"}
+            },
+            cells: {
+                values: cell_vals,
+                align: "center",
+                height: 30,
+                line: {color: "black", width: 4},
+                fill: {color: "#666699"},
+                font: {family: "Arial", size: 12, color: ["white"]}
+            }
+        }],
+        layout: {
+            title: 'Latest SpaceX Mission',
+            title_color: "white",
+            autosize: true,
+            height: 300,
+            paper_bgcolor: '#000000',
+            plot_bgcolor: '#000000'
+        }
+    };
+    return launch_data;
+}
+
+
+function get_vals_allPast(json){
+    var data = JSON.parse(json);
+    var cell_vals = data_sort(data);
+    var launch_data = {
+        upcoming_launches: [{
+            type: 'table',
+            columnwidth: [30, 30, 45, 30, 30, 30, 40],
+            header: {
+                values: [["Flight Number"], ["Mission Name"], ["Launch Date"],
+                			["Cargo"], ["Cargo Weight"], ["Cargo Owner"], ["Launch Site"]],
+                align: "center",
+                height: 40,
+                line: {width: 4, color: 'black'},
+                fill: {color: "#ffb200"},
+                font: {family: "Arial", size: 18, color: "white"}
+            },
+            cells: {
+                values: cell_vals,
+                align: "center",
+                height: 30,
+                line: {color: "black", width: 4},
+                fill: {color: "#666699"},
+                font: {family: "Arial", size: 12, color: ["white"]}
+            }
+        }],
+        layout: {
+            title: 'Full Launch Mission History     (Scroll to see all)',
+            autosize: true,
+            height: 3700,
+            paper_bgcolor: '#000000',
+            plot_bgcolor: '#000000'
+        }
+    };
+    return launch_data;
+}
+
+
+function get_vals_allPast_failed(json){
+    var data = JSON.parse(json);
+    var cell_vals = data_sort_2(data);
+    var launch_data = {
+        upcoming_launches: [{
+            type: 'table',
+            columnwidth: [30, 30, 50, 30, 30, 30, 30,100],
+            header: {
+                values: [["Flight Number"], ["Mission Name"], ["Launch Date"],
+                			["Cargo"], ["Cargo Weight"], ["Cargo Owner"], ["Launch Site"], ["Details"]],
+                align: "center",
+                height: 40,
+                line: {width: 4, color: 'black'},
+                fill: {color: "#ffb200"},
+                font: {family: "Arial", size: 18, color: "white"}
+            },
+            cells: {
+                values: cell_vals,
+                align: "center",
+                height: 30,
+                line: {color: "black", width: 4},
+                fill: {color: "#666699"},
+                font: {family: "Arial", size: 12, color: ["white"]}
+            }
+        }],
+        layout: {
+            title: 'Failed Missions',
+            autosize: true,
+            height: 700,
+            paper_bgcolor: '#000000',
+            plot_bgcolor: '#000000'
+        }
+    };
+    return launch_data;
+}
+
+function site_count(data){
+    
+}
+
+
+function launch_site_pie(json){
+    var data = JSON.parse(json);
+    var cell_val = data;
+    var pie = {
+        data: [{
+            values: cell_val,
+            labels: ['Kwajalein Atoll Omelek Island', 
+                'Cape Canaveral Air Force Station Space Launch Complex 40', 
+              'Vandenberg Air Force Base Space Launch Complex 4E', 
+              'Kennedy Space Center Historic Launch Complex 39A'],
+            type: 'pie'
+        }],
+        layout: {
+            title: 'Utilized Launch Sites',
+            autosize: true,
+            height: 600,
+            width: 1000,
+            color: 'white',
+            paper_bgcolor: '#000000',
+            plot_bgcolor: '#000000'
+        }
+    }
+    return pie;
+}
---
+++ server2.py
@@ -0,0 +1,120 @@
+import urllib.request
+import requests
+import json
+import bottle
+import launches
+import input_handling
+from bottle import route, post, run, static_file, request, redirect
+
+# *****************************************************
+# ***********************VARIABLES*********************
+
+url1 = "https://api.spacexdata.com/v3/launches/upcoming?limit=10"
+url2 = "https://api.spacexdata.com/v3/launches/past"
+url3 = "https://api.spacexdata.com/v3/launches/past?limit=10"
+url4 = "https://api.spacexdata.com/v3/launches/latest"
+
+
+# *********************** INDEXES *********************
+
+
+@route('/')
+def get_index():
+    return static_file("indexHome.html", root='')
+
+
+@route('/about')
+def get_indexAbout():
+    return static_file("indexAbout.html", root='')
+
+
+@route('/LH')
+def get_indexLH():
+    return static_file("indexLH.html", root='')
+
+
+@route('/failed_page')
+def get_indexFail():
+    return static_file("indexFail.html", root='')
+
+
+@route('/info')
+def get_indexInfo():
+    return static_file("indexInfo.html", root='')
+
+
+@route('/robots.txt')
+def get_permission():
+    return static_file("robots", root='')
+
+
+# ********** END INDEXES **********
+
+
+# ********** SUPPORT FILES **********
+
+
+@route('/LHlogo')
+def logo():
+    return static_file("LHlogo.png", root='')
+
+
+@route('/tabimg')
+def favicon():
+    return static_file("favicon_sat.png", root='')
+
+
+@route('/charts')
+def graphes():
+    return static_file("charts.js", root='')
+
+# ********** END SUPPORT FILES **********
+
+# ********** SORTED LAUNCH FILES **********
+
+
+@route('/upcoming')
+def get_upcoming_launches():
+    return launches.get_launch_data(url1)
+
+
+@route('/past')
+def get_past10_launches():
+    return launches.get_launch_data_past_sort(url2)
+
+
+@route('/allPast')
+def get_all_past_launches():
+    return launches.get_launch_data(url2)
+
+
+@route('/latest')
+def get_latest_launches():
+    return launches.get_latest_launch_data(url4)
+
+
+@route('/failed')
+def get_failed_launches_data():
+    return launches.get_failed_launches(url2)
+
+
+@route('/site_count')
+def get_site_count():
+    return launches.launch_locations(url2)
+
+
+# ********** POST **********
+
+
+@post('/about')
+def do_submit():
+    print('confirmed')
+    content = request.forms.get('email')
+    input_handling.add_email(content)
+    redirect('/about')
+
+
+# ********** BOTTLE RUN **********
+
+
+bottle.run(host='0.0.0.0', port=8080, debug=True)
